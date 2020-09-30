fa2f2c73ffc6035d0b4b56caaa66e3ee48b8a301
EliasCardon <62133614+EliasCardon@users.noreply.github.com>
2020-09-30 14:37:17 +0200

    MàJ

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>La Bonne Planque</title>
+	<link rel="stylesheet" type="text/css" href="src/css/style.css">
+</head>
+<body>
+	<header>
+		<?php include("src/include/header.php") ?>
+	</header>
+
+	<footer>
+    <?php include("src/include/footer.php") ?>
+</footer>
+</body>
+</html>
