40a363b0e1f802a96cec07f77ad40a89487b928e
EliasCardon <62133614+EliasCardon@users.noreply.github.com>
2020-10-07 22:01:46 +0200

    Update CGU.php

--- CGU.php
+++ CGU.php
@@ -17,7 +17,7 @@
         <h2 class="card-title h2">Conditions générales d'utilisation</h2>
 
         <!-- Subtitle -->
-        <p class="my-4 h6">Les articles en vente sur Candysan</p>
+        <p class="my-4 h6">Les articles en vente sur La Bonne Planque</p>
 
         <!-- Grid row -->
         <div class="row d-flex justify-content-center">
@@ -25,7 +25,10 @@
             <!-- Grid column -->
             <div class="col-xl-7 pb-2">
 
-                <p class="card-text">Tous les articles vendus sur Candysan sont des articles originaux achetés et expédiés depuis le Japon. Tous les produits sont destinés au marché japonais et respectent les normes et la législation en vigueur au Japon. Dans la grande majorité des cas les informations et conseils d'utilisation inscrits sur les emballages sont en japonais uniquement.</p>
+                <p class="card-text">Tous les articles vendus sur Candysan sont des articles originaux achetés et
+                    expédiés depuis Nos locaux à Marseille. Tous les produits sont destinés au marché français et
+                    respectent les normes et la législation en vigueur en France. Dans la grande majorité des cas les
+                    informations et conseils d'utilisation inscrits sur les emballages sont en français uniquement.</p>
 
             </div>
             <!-- Grid column -->
@@ -34,7 +37,293 @@
         <!-- Grid row -->
 
         <hr class="my-4 rgba-white-light">
-        
+
+        <!-- Subtitle -->
+        <p class="my-4 h6">Le prix des articles</p>
+
+        <!-- Grid row -->
+        <div class="row d-flex justify-content-center">
+
+            <!-- Grid column -->
+            <div class="col-xl-7 pb-2">
+
+                <p class="card-text">Tout les tarifs sont affichés en Euro (€).</p>
+
+            </div>
+            <!-- Grid column -->
+
+        </div>
+        <!-- Grid row -->
+        <hr class="my-4 rgba-white-light">
+
+        <!-- Subtitle -->
+        <p class="my-4 h6">L'étiquetage des produits</p>
+
+        <!-- Grid row -->
+        <div class="row d-flex justify-content-center">
+
+            <!-- Grid column -->
+            <div class="col-xl-7 pb-2">
+
+                <p class="card-text">Bien que nous fassions tout notre possible pour nous assurer que les informations
+                    de nos produits
+                    soient correctes, les fabricants peuvent modifier occasionnellement leurs étiquetages alimentaires.
+
+                    Les emballages peuvent notamment contenir différentes et/ou plus d’informations que sur La Bonne
+                    Planque.
+
+                    Nous recommandons de ne pas vous fier uniquement aux informations de notre site et de toujours
+                    vérifier directement les informations (ingrédients, allergènes, mode de préparation, etc.) figurant
+                    sur les emballages des produits avant de les consommer. Pour de plus amples informations sur un
+                    produit, contactez directement le fabricant.
+
+                    Les informations figurant sur La Bonne Planque ne le sont qu’à titre indicatif et ne peuvent en
+                    aucun cas se
+                    substituer aux conseils d’un médecin, d’un pharmacien ou tout autre membre agréé de la profession
+                    médicale.
+
+                    Il est par ailleurs fortement déconseillé d’utiliser ces informations à des fins d’auto-diagnostic
+                    ou pour traiter un problème de santé ou une maladie. Contactez immédiatement votre médecin traitant
+                    si vous soupçonnez un problème médical.
+
+                    La Bonne Planque ne pourra être tenu pour responsable en cas d’erreur ou inexactitude quant aux
+                    informations
+                    figurant sur le site.</p>
+
+            </div>
+            <!-- Grid column -->
+
+        </div>
+        <!-- Grid row -->
+
+        <hr class="my-4 rgba-white-light">
+
+        <!-- Subtitle -->
+        <p class="my-4 h6">Photos des produits</p>
+
+        <!-- Grid row -->
+        <div class="row d-flex justify-content-center">
+
+            <!-- Grid column -->
+            <div class="col-xl-7 pb-2">
+
+                <p class="card-text">Les photos des produits affichées sur La Bonne Planque sont parfois des photos
+                    prises par nos soins mais le plus souvent nous utilisons des photos mises à disposition par les
+                    fabricants ou les distributeurs. L'objet final peut donc être légèrement différent des photos. </p>
+
+            </div>
+            <!-- Grid column -->
+
+        </div>
+        <!-- Grid row -->
+
+        <hr class="my-4 rgba-white-light">
+
+        <!-- Subtitle -->
+        <p class="my-4 h6">Commentaires des produits</p>
+
+        <!-- Grid row -->
+        <div class="row d-flex justify-content-center">
+
+            <!-- Grid column -->
+            <div class="col-xl-7 pb-2">
+
+                <p class="card-text">Nous offrons la possibilité aux internautes de commenter en toute liberté nos
+                    articles sur leurs fiches produits. Cependant, nous nous réservons le droit de modérer les
+                    commentaires ne correspondant pas aux règles de la bienséance, mais également tout commentaires hors
+                    sujet, répétés plusieurs fois ou promotionnels (publicité pour un site externe, transmission du code
+                    de parrainage, etc) ceci afin de préserver l'aspect pratique et informatif des commentaires. Toute
+                    utilisation abusive de cet outil pourra également être sanctionnée par la suspension du compte de(s)
+                    l'auteur(s).</p>
+
+            </div>
+            <!-- Grid column -->
+
+        </div>
+        <!-- Grid row -->
+
+        <hr class="my-4 rgba-white-light">
+
+        <!-- Subtitle -->
+        <p class="my-4 h6">Expédition des commandes</p>
+
+        <!-- Grid row -->
+        <div class="row d-flex justify-content-center">
+
+            <!-- Grid column -->
+            <div class="col-xl-7 pb-2">
+
+                <p class="card-text">Tous les produits disponibles sur La Bonne Planque sont stockés dans nos locaux. De
+                    cette façon, nous sommes en mesure d'envoyer vos commandes plus rapidement. Nous sommes une
+                    entreprise de taille humaine et pour nous chaque client est important. C'est pourquoi nous portons
+                    une attention particulière à nos emballages qui protègent efficacement les objets durant le
+                    transport. C'est pour cela également que nous expédions les commandes le plus rapidement possible
+                    afin que nos colis arrivent à destination dans les délais les plus brefs.</p>
+
+            </div>
+            <!-- Grid column -->
+
+        </div>
+        <!-- Grid row -->
+
+        <hr class="my-4 rgba-white-light">
+
+        <!-- Subtitle -->
+        <p class="my-4 h6">Produits abîmés ou égarés durant le transport</p>
+
+        <!-- Grid row -->
+        <div class="row d-flex justify-content-center">
+
+            <!-- Grid column -->
+            <div class="col-xl-7 pb-2">
+
+                <p class="card-text">Si vous avez choisi un mode d'expédition suivi avec assurance nous pourrons vous
+                    rembourser vos achats après l'enquête du transporteur. Celle-ci peut prendre plusieurs semaines.
+                    Aucun remboursement ni recours ne sera possible dans le cas d'un paquet expédié en DPD car il n'est
+                    ni suivi ni assuré.</p>
+
+            </div>
+            <!-- Grid column -->
+
+        </div>
+        <!-- Grid row -->
+
+        <hr class="my-4 rgba-white-light">
+
+        <!-- Subtitle -->
+        <p class="my-4 h6">Produits défectueux d'origine</p>
+
+        <!-- Grid row -->
+        <div class="row d-flex justify-content-center">
+
+            <!-- Grid column -->
+            <div class="col-xl-7 pb-2">
+
+                <p class="card-text">Lorsque cela est possible nous contrôlons soigneusement les produits avant
+                    l'expédition et portons toujours un grand soin à l'emballage. Si toutefois vous recevez un produit
+                    défectueux veuillez nous envoyer des photos présentant le problème. Nous vous renverrons le produit
+                    le plus vite possible en utilisant le mode de transport que vous avez choisi initialement.</p>
+
+            </div>
+            <!-- Grid column -->
+
+        </div>
+        <!-- Grid row -->
+
+        <hr class="my-4 rgba-white-light">
+
+        <!-- Subtitle -->
+        <p class="my-4 h6">Produits que vous ne voulez plus</p>
+
+        <!-- Grid row -->
+        <div class="row d-flex justify-content-center">
+
+            <!-- Grid column -->
+            <div class="col-xl-7 pb-2">
+
+                <p class="card-text">Nous n'acceptons pas les retours car il est généralement impossible de remettre en
+                    vente de la nourriture qui a déjà voyagé. Les annulations de commandes sont possibles si vous nous
+                    prévenez rapidement et que nous voyons votre message avant que votre/vos colis ne soit
+                    expédié(s).</p>
+
+            </div>
+            <!-- Grid column -->
+
+        </div>
+        <!-- Grid row -->
+
+        <hr class="my-4 rgba-white-light">
+
+        <!-- Subtitle -->
+        <p class="my-4 h6">Colis retournés par la Poste</p>
+
+        <!-- Grid row -->
+        <div class="row d-flex justify-content-center">
+
+            <!-- Grid column -->
+            <div class="col-xl-7 pb-2">
+
+                <p class="card-text">Dans le cas où un paquet est retourné par la poste nous ré-expédions le colis sans
+                    frais dans les cas où :<br/><br/>
+
+
+                    - La Bonne Planque a commis une erreur (erreur dans l'adresse par exemple)<br/><br/>
+
+
+                    - la Poste a commis une erreur (colis renvoyé sans justificatif par exemple)<br/><br/>
+
+
+                    - le colis est renvoyé à cause d'un problème technique (étiquette arrachée ou rendue illisible par
+                    exemple).<br/><br/>
+
+
+                    Dans tous les autres cas, la ré-expédition se fera aux frais du destinataire sauf s'il choisi la
+                    formule d'expédition comprenant l'assurance La Bonne Planque. Les colis qui nous reviennent sont
+                    conservés
+                    30 jours. Cette période passée nous ne pouvons plus les ré-expédier.<br/><br/>
+
+
+                    Attention, les colis expédiés en Postal Parcel (ex : Martinique, Guadeloupe, Polynésie Française...)
+                    ne peuvent en aucun cas être réexpédiés en cas de problème de livraison. Pensez à bien vérifier le
+                    suivit de votre colis en ligne.</p>
+
+            </div>
+            <!-- Grid column -->
+
+        </div>
+        <!-- Grid row -->
+
+        <hr class="my-4 rgba-white-light">
+
+        <!-- Subtitle -->
+        <p class="my-4 h6">Assurance La Bonne Planque</p>
+
+        <!-- Grid row -->
+        <div class="row d-flex justify-content-center">
+
+            <!-- Grid column -->
+            <div class="col-xl-7 pb-2">
+
+                <p class="card-text">L’assurance La Bonne Planque couvre les problèmes de livraison sur les paquets Colissimo et
+                    "UPS".<br/><br/>
+
+
+                    Si une commande n’arrive pas à destination ou est retournée à nos locaux en tout ou en partie, nous la
+                    renvoyons à nos frais et en remplaçant les produits abîmés durant le transport ainsi que ceux ayant
+                    une date de péremption trop proche.<br/><br/>
+
+
+                    L’assurance La Bonne Planque fonctionne dans tous les cas lorsqu’un colis est perdu ou retourné à nos locaux à
+                    cause d’un problème de livraison, peu importe le motif de retour. Exemples : paquet non réclamé,
+                    adresse incomplète, adresse introuvable, fausse direction…<br/><br/>
+
+
+                    L’assurance ne s’applique pas si le client refuse un paquet volontairement à moins que ce dernier ne
+                    soit endommagé.<br/><br/>
+
+
+                    Si un colis est retourné à nos locaux, le délai de retour est de 1 à 3 mois à partir de la date de la
+                    commande. Le colis est envoyé à nouveau dès qu’il nous revient après confirmation de l’adresse de
+                    livraison par le client si cela est nécessaire.<br/><br/>
+
+
+                    Un colis est considéré comme perdu s’il n’est ni arrivé à destination ni retourné à nos locaux après 90
+                    jours (3 mois). Passé ce délai, toute réclamation devra être faite sous 30 jours (1 mois), la
+                    commande pourra alors être de nouveau expédiée ou remboursée sous forme d’avoir. Le délai de
+                    réclamation passé, aucune demande de dédommagement ne pourra être acceptée.<br/><br/>
+
+
+                    L’assurance La Bonne Planque fonctionne sur un maximum de 3 colis par commande, soit sur les commandes dont
+                    les frais de port sont égaux ou inférieurs à environ 62 euros.<br/><br/>
+
+
+                    L’assurance ne peut être utilisée qu’une seule fois par commande.</p>
+
+            </div>
+            <!-- Grid column -->
+
+        </div>
+        <!-- Grid row -->
 
     </div>
     <!-- Jumbotron -->
