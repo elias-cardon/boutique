ea9a1b353144ecde4cdaeec07e8015728676e2f4
EliasCardon <62133614+EliasCardon@users.noreply.github.com>
2020-10-09 18:30:48 +0200

    Update contact.php

--- contact.php
+++ contact.php
@@ -25,22 +25,22 @@
         <!-- Default form contact -->
         <form class="text-center" action="#!">
 
-         <h3 class="font-weight-bold mb-4">Contact Us</h3>
+         <h3 class="font-weight-bold mb-4">Contactez nous</h3>
 
           <!-- Name -->
-          <input type="text" id="defaultContactFormName" class="form-control mb-4" placeholder="Name">
+          <input type="text" id="defaultContactFormName" class="form-control mb-4" placeholder="Nom">
 
           <!-- Email -->
           <input type="email" id="defaultContactFormEmail" class="form-control mb-4" placeholder="E-mail">
 
           <!-- Subject -->
-          <label>Subject</label>
+          <label>Sujet du message</label>
           <select class="browser-default custom-select mb-4">
-            <option value="" disabled>Choose option</option>
-            <option value="1" selected>Feedback</option>
-            <option value="2">Report a bug</option>
-            <option value="3">Feature request</option>
-            <option value="4">Feature request</option>
+            <option value="" disabled>Choisissez une option</option>
+            <option value="1" selected>Commande</option>
+            <option value="2">Livraison</option>
+            <option value="3">Assurance</option>
+            <option value="4">Autre</option>
           </select>
 
           <!-- Message -->
@@ -52,11 +52,11 @@
           <!-- Copy -->
           <div class="custom-control custom-checkbox mb-4">
             <input type="checkbox" class="custom-control-input" id="defaultContactFormCopy">
-            <label class="custom-control-label" for="defaultContactFormCopy">Send me a copy of this message</label>
+            <label class="custom-control-label" for="defaultContactFormCopy">Envoyer une copie de cet email</label>
           </div>
 
           <!-- Send button -->
-          <button class="btn btn-info btn-block" type="submit">Send</button>
+          <button class="btn btn-info btn-block" type="submit">Envoyer</button>
 
         </form>
         <!-- Default form contact -->
