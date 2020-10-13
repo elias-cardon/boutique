5a3c548d8268d0ef51c9e284db971b36243daba5
EliasCardon <62133614+EliasCardon@users.noreply.github.com>
2020-10-13 12:49:54 +0200

    MàJ

--- include/footer.php
+++ include/footer.php
@@ -2,76 +2,72 @@
 <html>
 <head>
     <title></title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
-          integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="src/css/style.css">
 </head>
 <body>
 <!-- Footer -->
-<!-- FOOTER START -->
-<div class="footer">
-  <div class="contain">
-  <div class="col">
-    <h1>L'entreprise</h1>
-    <ul>
-      <li><a href="propos.php">A propos</a></li>
-      <li><a href="faq.php">FAQ</a></li>
-      <li><a href="engagements.php">Nos engagements</a></li>
-      <li><a href="livraison.php">Livraisons</a></li>
-      <li><a href="CGU.php">CGU</a></li>
-    </ul>
-  </div>
-  <div class="col">
-    <h1>Products</h1>
-    <ul>
-      <li>About</li>
-      <li>Mission</li>
-      <li>Services</li>
-      <li>Social</li>
-      <li>Get in touch</li>
-    </ul>
-  </div>
-  <div class="col">
-    <h1>Accounts</h1>
-    <ul>
-      <li>About</li>
-      <li>Mission</li>
-      <li>Services</li>
-      <li>Social</li>
-      <li>Get in touch</li>
-    </ul>
-  </div>
-  <div class="col">
-    <h1>Resources</h1>
-    <ul>
-      <li>Webmail</li>
-      <li>Redeem code</li>
-      <li>WHOIS lookup</li>
-      <li>Site map</li>
-      <li>Web templates</li>
-      <li>Email templates</li>
-    </ul>
-  </div>
-  <div class="col">
-    <h1>Support</h1>
-    <ul>
-      <li><a href="contact.php">Contactez-nous</a></li>
-    </ul>
-  </div>
-<div class="clearfix"></div>
-</div>
-</div>
-<!-- END OF FOOTER -->
+<footer>
+  <nav class="footer-inner">
+    <section class="footer-item">
+      <h1>La Bonne Planque</h1>
+      
+      <h2>We create possibilities <br>for the connected world.<br><b class="color">Be Bold.</b></h2>
+    </section>
+        
+    <section class="footer-item">
+      <h3>Explore</h3>
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Capabilities</a></li>
+          <li><a href="#">Careers</a></li>
+        </ul>
+    </section>
+          
+    <section class="footer-item">
+      <h3>Visit</h3>
+        <a href="#">
+        <p>Envoy So. California</p>
+        <p>34 Tesla, Ste 100</p>
+        <p>Irvine, Ca, USA 92618</p>
+        </a>
+            
+      <h3 class="desktop">New Business</h3>
+        <p class="desktop"><a href="#">engage@weareenvoy.com</a></p>
+        <p class="desktop"><a href="#">949.333.3106</a></p>
+    </section>
+            
+    <section class="footer-item">
+      <h3>New Business</h3>
+        <p><a href="#">Email us</a></p>
+        <p><a href="#">949.333.3106</a></p>
+    </section>
+        
+    <section class="footer-item">
+      <h3>Follow</h3>
+        <ul>
+          <li><a href="#">Instagram</a></li>
+          <li><a href="#">Twitter</a></li>
+          <li><a href="#">LinkedIn</a></li>
+        </ul>
+    </section>
+        
+    <section class="footer-item">
+      <h3>Legal</h3>
+        <ul>
+          <li><a href="#">Terms</a></li>
+          <li><a href="#">Privacy</a></li>
+        </ul>
+    </section>
+            
+    <section class="footer-item">
+        <a href="#" class="footer-button">Next: About</a>
+    </section>
+  </nav>
+</footer>
 <!-- Footer -->
 
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
-        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
-        crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"
-        integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN"
-        crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"
-        integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV"
-        crossorigin="anonymous"></script>
+
 </body>
 </html>
 
--- include/header.php
+++ include/header.php
@@ -4,32 +4,42 @@
     <link rel="stylesheet" type="text/css" href="src/css/style.css">
 </head>
 <body>
-<header class="header-area">
-    <!-- site-navbar start -->
-    <div class="navbar-area">
-        <div class="container">
-            <nav class="site-navbar">
-                <!-- site logo -->
-                <a href="#home" class="site-logo">logo</a>
+<header>
+    <header class="header-area">
+        <!-- site-navbar start -->
+        <div class="navbar-area">
+            <div class="container">
+                <nav class="site-navbar">
+                    <!-- site logo -->
+                    <a href="#home" class="site-logo">La Bonne Planque</a>
 
-                <!-- site menu/nav -->
-                <ul>
-                    <li><a href="index.php">home</a></li>
-                    <li><a href="#">about</a></li>
-                    <li><a href="#">service</a></li>
-                    <li><a href="#">contact</a></li>
-                </ul>
+                    <!-- site menu/nav -->
+                    <ul>
+                        <li><a href="index.php">Accueil</a></li>
+                        <li><a href="boutique.php">Boutique</a></li>
+                        <li><a href="panier.php">Panier</a></li>
+                        <li><a href="inscription.php">Inscription</a></li>
+                        <li><a href="connexion.php">Connexion</a></li>
+                    </ul>
 
-                <!-- nav-toggler for mobile version only -->
-                <button class="nav-toggler">
-                    <span></span>
-                </button>
-            </nav>
-        </div>
-    </div><!-- navbar-area end -->
-</header>
+                    <!-- nav-toggler for mobile version only -->
+                    <button class="nav-toggler">
+                        <span></span>
+                    </button>
+                </nav>
+            </div>
+        </div><!-- navbar-area end -->
+    </header>
 
 
-<script src="src/js/script.js"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+            integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
+            crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"
+            integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN"
+            crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"
+            integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV"
+            crossorigin="anonymous"></script>
 </body>
 </html>
