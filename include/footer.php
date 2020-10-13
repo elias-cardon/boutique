d97282eb6440525211a78a3b3743e069d12682b7
EliasCardon <62133614+EliasCardon@users.noreply.github.com>
2020-10-13 11:42:26 +0200

    MàJ

--- include/footer.php
+++ include/footer.php
@@ -1,111 +1,77 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
+          integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+</head>
+<body>
 <!-- Footer -->
-<footer class="page-footer font-small indigo">
-
-    <!-- Footer Links -->
-    <div class="container text-center text-md-left">
-
-        <!-- Grid row -->
-        <div class="row">
-
-            <!-- Grid column -->
-            <div class="col-md-3 mx-auto">
-
-                <!-- Links -->
-                <h5 class="font-weight-bold text-uppercase mt-3 mb-4">Informations</h5>
-
-                <ul class="list-unstyled">
-                    <li>
-                        <a href="propos.php">A propos de La Bonne Planque</a>
-                    </li>
-                    <li>
-                        <a href="livraison.php">Livraisons</a>
-                    </li>
-                    <li>
-                        <a href="CGU.php">Conditions générales d'utilisation</a>
-                    </li>
-                    <li>
-                        <a href="engagement.php">Nos engagements</a>
-                    </li>
-                    <li>
-                        <a href="faq.php">FAQ</a>
-                    </li>
-                </ul>
-
-            </div>
-            <!-- Grid column -->
-
-            <hr class="clearfix w-100 d-md-none">
-
-            <!-- Grid column -->
-            <div class="col-md-3 mx-auto">
-
-                <!-- Links -->
-                <h5 class="font-weight-bold text-uppercase mt-3 mb-4">Nos produits</h5>
-
-                <ul class="list-unstyled">
-                    <li>
-                        <a href="#!">Nouveaux Produits</a>
-                    </li>
-                    <li>
-                        <a href="#!">Meilleures ventes</a>
-                    </li>
-                    <li>
-                        <a href="#!">En promotion</a>
-                    </li>
-                </ul>
-
-            </div>
-            <!-- Grid column -->
-
-            <hr class="clearfix w-100 d-md-none">
-
-            <!-- Grid column -->
-            <div class="col-md-3 mx-auto">
-
-                <!-- Links -->
-                <h5 class="font-weight-bold text-uppercase mt-3 mb-4">Notre boutique</h5>
-
-                <ul class="list-unstyled">
-                    <li>
-                        <a href="contact.php">Contactez-nous</a>
-                    </li>
-                </ul>
-
-            </div>
-            <!-- Grid column -->
-
-            <hr class="clearfix w-100 d-md-none">
-
-            <!-- Grid column -->
-            <div class="col-md-3 mx-auto">
-
-                <!-- Links -->
-                <h5 class="font-weight-bold text-uppercase mt-3 mb-4">Votre compte</h5>
-
-                <ul class="list-unstyled">
-                    <li>
-                        <a href="#!">Informations personnelles</a>
-                    </li>
-                    <li>
-                        <a href="#!">Commandes</a>
-                    </li>
-                    <li>
-                        <a href="#!">Panier</a>
-                    </li>
-                </ul>
-
-            </div>
-            <!-- Grid column -->
-
-        </div>
-        <!-- Grid row -->
-
-    </div>
-    <!-- Footer Links -->
-
-    <!-- Copyright -->
-    <div class="footer-copyright text-center py-3">© 2020 Copyright - La Bonne Planque</div>
-    <!-- Copyright -->
-
-</footer>
+<!-- FOOTER START -->
+<div class="footer">
+  <div class="contain">
+  <div class="col">
+    <h1>L'entreprise</h1>
+    <ul>
+      <li><a href="propos.php">A propos</a></li>
+      <li><a href="faq.php">FAQ</a></li>
+      <li><a href="engagements.php">Nos engagements</a></li>
+      <li><a href="livraison.php">Livraisons</a></li>
+      <li><a href="CGU.php">CGU</a></li>
+    </ul>
+  </div>
+  <div class="col">
+    <h1>Products</h1>
+    <ul>
+      <li>About</li>
+      <li>Mission</li>
+      <li>Services</li>
+      <li>Social</li>
+      <li>Get in touch</li>
+    </ul>
+  </div>
+  <div class="col">
+    <h1>Accounts</h1>
+    <ul>
+      <li>About</li>
+      <li>Mission</li>
+      <li>Services</li>
+      <li>Social</li>
+      <li>Get in touch</li>
+    </ul>
+  </div>
+  <div class="col">
+    <h1>Resources</h1>
+    <ul>
+      <li>Webmail</li>
+      <li>Redeem code</li>
+      <li>WHOIS lookup</li>
+      <li>Site map</li>
+      <li>Web templates</li>
+      <li>Email templates</li>
+    </ul>
+  </div>
+  <div class="col">
+    <h1>Support</h1>
+    <ul>
+      <li><a href="contact.php">Contactez-nous</a></li>
+    </ul>
+  </div>
+<div class="clearfix"></div>
+</div>
+</div>
+<!-- END OF FOOTER -->
 <!-- Footer -->
+
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
+        crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"
+        integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN"
+        crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"
+        integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV"
+        crossorigin="anonymous"></script>
+</body>
+</html>
+
--- include/header.php
+++ include/header.php
@@ -1,69 +1,35 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
-          integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="src/css/style.css">
 </head>
 <body>
-<header>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
-        <a class="navbar-brand" href="#">La Bonne Planque</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown"
-                aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNavDropdown">
-            <ul class="navbar-nav">
-                <li class="nav-item active">
-                    <a class="nav-link" href="index.php">Accueil <span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="boutique.php">Boutique</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="panier.php">Panier</a>
-                </li>
-                <?php
-                if (!isset($_SESSION['login'])) {
-                    ?>
-                    <li class="nav-item">
-                        <a class="nav-link" href="inscription.php">Inscription</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="connexion.php">Connexion</a>
-                    </li>
-                    <?php
-                }
-                ?>
-                <?php
-                $pageSelected = 'profil';
-                if (isset($_SESSION['login'])) {
-                    ?>
+<header class="header-area">
+    <!-- site-navbar start -->
+    <div class="navbar-area">
+        <div class="container">
+            <nav class="site-navbar">
+                <!-- site logo -->
+                <a href="#home" class="site-logo">logo</a>
 
-                        <ul>
-                            <li class="btn"><a href="profil.php">profil</a></li>
-                            <?php if ($pageSelected == 'profil') { ?>
-                                <li class="btn"><a href="admin.php">Admin</a></li>
-                            <?php } ?>
-                            <li class="btn"><a href="logout.php">Déconnexion</a></li>
-                        </ul>
-                    <?php
-                }
-                ?>
-            </ul>
+                <!-- site menu/nav -->
+                <ul>
+                    <li><a href="index.php">home</a></li>
+                    <li><a href="#">about</a></li>
+                    <li><a href="#">service</a></li>
+                    <li><a href="#">contact</a></li>
+                </ul>
+
+                <!-- nav-toggler for mobile version only -->
+                <button class="nav-toggler">
+                    <span></span>
+                </button>
+            </nav>
         </div>
-    </nav>
+    </div><!-- navbar-area end -->
 </header>
 
 
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
-        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
-        crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"
-        integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN"
-        crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"
-        integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV"
-        crossorigin="anonymous"></script>
+<script src="src/js/script.js"></script>
 </body>
 </html>
