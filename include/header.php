5a7a4223ffd53f167cb49b2e2e4f1c5b0bd66a34
EliasCardon <62133614+EliasCardon@users.noreply.github.com>
2020-10-15 12:27:24 +0200

    MàJ

--- include/header.php
+++ include/header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <link rel="stylesheet" type="text/css" href="src/css/header.css">
+    <link rel="stylesheet" type="text/css" href="src/css/style.css">
 </head>
 <body>
 <header>
