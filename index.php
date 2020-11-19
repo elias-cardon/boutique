2c1c2cfbf43d803363d1763ef7673ec4479c47c6
EliasCardon <62133614+EliasCardon@users.noreply.github.com>
2020-11-19 14:31:15 +0100

    Update index.php

--- index.php
+++ index.php
@@ -48,7 +48,7 @@
 					 <a href="details.php?pdid=<?php echo $newpd['pid'];?>"><img src="admin/<?php echo $newpd['image']; ?>" alt="" /></a>
 					 <h2><?php echo $newpd['productName'];?></h2>
 					 <p><?php echo $fm->textShorten($newpd['body'], 50);?></p>
-					 <p><span class="price">$ <?php echo $newpd['price'];?></span></p>
+					 <p><span class="price">€ <?php echo $newpd['price'];?></span></p>
 				     <div class="button"><span><a href="details.php?pdid=<?php echo $newpd['pid'];?>" class="details">Details</a></span></div>
 				</div>
 			<?php
