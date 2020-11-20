2160880a6a79641ea3e4834c361718ad6790fa5c
EliasCardon <62133614+EliasCardon@users.noreply.github.com>
2020-11-20 15:48:53 +0100

    Suppression de fichiers inutiles

--- include/header.php
+++ include/header.php
@@ -28,7 +28,7 @@
 
 <!DOCTYPE HTML>
 <head>
-<title>Store Website</title>
+<title>La Bonne Planque</title>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 <link href="src/css/style.css" rel="stylesheet" type="text/css" media="all"/>
