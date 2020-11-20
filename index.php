094eabb54834ca0ae49e1bc17c3be3b7b5e52a66
EliasCardon <62133614+EliasCardon@users.noreply.github.com>
2020-11-20 15:05:20 +0100

    MàJ

--- index.php
+++ index.php
@@ -47,9 +47,34 @@
 				<div class="grid_1_of_4 images_1_of_4">
 					 <a href="details.php?pdid=<?php echo $newpd['pid'];?>"><img src="admin/<?php echo $newpd['image']; ?>" alt="" /></a>
 					 <h2><?php echo $newpd['productName'];?></h2>
-					 <p><?php echo $fm->textShorten($newpd['body'], 50);?></p>
 					 <p><span class="price">€ <?php echo $newpd['price'];?></span></p>
-				     <div class="button"><span><a href="details.php?pdid=<?php echo $newpd['pid'];?>" class="details">Details</a></span></div>
+				     <div class="button"><span><a href="details.php?pdid=<?php echo $newpd['pid'];?>" class="details">Voir plus</a></span></div>
+				</div>
+			<?php
+					}
+	      		}else{
+	      			echo "<span class='error'>No product found.</span>";
+	      		}
+			?>
+				
+			</div>
+			<div class="content_bottom">
+    		<div class="heading">
+    		<h3>Nos produits</h3>
+    		</div>
+    		<div class="clear"></div>
+    	</div>
+			<div class="section group">
+			<?php
+      		$getNewPd = $pd->getAllProduct();
+      		if ($getNewPd) {
+      			while ($newpd = $getNewPd->fetch_assoc()) {
+	      	?>
+				<div class="grid_1_of_4 images_1_of_4">
+					 <a href="details.php?pdid=<?php echo $newpd['pid'];?>"><img src="admin/<?php echo $newpd['image']; ?>" alt="" /></a>
+					 <h2><?php echo $newpd['productName'];?></h2>
+					 <p><span class="price">€ <?php echo $newpd['price'];?></span></p>
+				     <div class="button"><span><a href="details.php?pdid=<?php echo $newpd['pid'];?>" class="details">Voir plus</a></span></div>
 				</div>
 			<?php
 					}
