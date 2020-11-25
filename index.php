5fd02be839af103f8b824527e50d9e46ebb6c67c
EliasCardon <62133614+EliasCardon@users.noreply.github.com>
2020-11-25 10:56:41 +0100

    Update index.php

--- index.php
+++ index.php
@@ -46,7 +46,7 @@
 				<div class="grid_1_of_4 images_1_of_4">
 					 <a href="details.php?pdid=<?php echo $newpd['pid'];?>"><img src="admin/<?php echo $newpd['image']; ?>" alt="" /></a>
 					 <h2><?php echo $newpd['productName'];?></h2>
-					 <p><span class="price">€ <?php echo $newpd['price'];?></span></p>
+					 <p><span class="price"><?php echo $newpd['price'];?> €</span></p>
 				     <div class="button"><span><a href="details.php?pdid=<?php echo $newpd['pid'];?>" class="details">Voir plus</a></span></div>
 				</div>
 			<?php
