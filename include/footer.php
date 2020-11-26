388b2e89a0097b53ff7ce5daa5fa36ca9863f063
EliasCardon <62133614+EliasCardon@users.noreply.github.com>
2020-11-26 17:52:11 +0100

    Reverse git

--- include/footer.php
+++ include/footer.php
@@ -1,134 +1,126 @@
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
-      integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
-        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
-        crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx"
-        crossorigin="anonymous"></script>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
 
 <!-- Footer -->
 <footer class="page-footer font-small mdb-color pt-4">
 
-    <!-- Footer Links -->
-    <div class="container text-center text-md-left">
+  <!-- Footer Links -->
+  <div class="container text-center text-md-left">
 
-        <!-- Footer links -->
-        <div class="row text-center text-md-left mt-3 pb-3">
+    <!-- Footer links -->
+    <div class="row text-center text-md-left mt-3 pb-3">
 
-            <!-- Grid column -->
-            <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
-                <h6 class="text-uppercase mb-4 font-weight-bold">La Bonne Planque</h6>
-                <p>Nous créons des possibilités
-                    pour le monde. Soyez audacieux.</p>
-            </div>
-            <!-- Grid column -->
+      <!-- Grid column -->
+      <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
+        <h6 class="text-uppercase mb-4 font-weight-bold">La Bonne Planque</h6>
+        <p>Nous créons des possibilités
+pour le monde. Soyez audacieux.</p>
+      </div>
+      <!-- Grid column -->
 
-            <hr class="w-100 clearfix d-md-none">
+      <hr class="w-100 clearfix d-md-none">
 
-            <!-- Grid column -->
-            <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
-                <h6 class="text-uppercase mb-4 font-weight-bold">A propos</h6>
-                <p>
-                    <a href="propos.php">A propos de nous</a>
-                </p>
-                <p>
-                    <a href="faq.php">FAQ</a>
-                </p>
-                <p>
-                    <a href="contact.php">Contact</a>
-                </p>
-            </div>
-            <!-- Grid column -->
+      <!-- Grid column -->
+      <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
+        <h6 class="text-uppercase mb-4 font-weight-bold">A propos</h6>
+        <p>
+          <a href="propos.php">A propos de nous</a>
+        </p>
+        <p>
+          <a href="faq.php">FAQ</a>
+        </p>
+      </div>
+      <!-- Grid column -->
 
-            <hr class="w-100 clearfix d-md-none">
+      <hr class="w-100 clearfix d-md-none">
 
-            <!-- Grid column -->
-            <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mt-3">
-                <h6 class="text-uppercase mb-4 font-weight-bold">Légal</h6>
-                <p>
-                    <a href="CGU.php">CGU</a>
-                </p>
-                <p>
-                    <a href="livraison.php">Livraison</a>
-                </p>
-            </div>
+      <!-- Grid column -->
+      <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mt-3">
+        <h6 class="text-uppercase mb-4 font-weight-bold">Légal</h6>
+        <p>
+          <a href="CGU.php">CGU</a>
+        </p>
+        <p>
+          <a href="livraison.php">Livraison</a>
+        </p>
+      </div>
 
-            <!-- Grid column -->
-            <hr class="w-100 clearfix d-md-none">
+      <!-- Grid column -->
+      <hr class="w-100 clearfix d-md-none">
 
-            <!-- Grid column -->
-            <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
-                <h6 class="text-uppercase mb-4 font-weight-bold">Contact</h6>
-                <p>
-                    <i class="fas fa-home mr-3"></i> 8 Rue d'hozier, 13002 Marseille</p>
-                <p>
-                    <i class="fas fa-envelope mr-3"></i> info@labonneplanque.com</p>
-                <p>
-                    <i class="fas fa-phone mr-3"></i>Tel. : (+33) 222 666 444</p>
-                <p>
-                    <i class="fas fa-print mr-3"></i>Fax : (+33) 000 00 00 0</p>
-            </div>
-            <!-- Grid column -->
-
-        </div>
-        <!-- Footer links -->
-
-        <hr>
-
-        <!-- Grid row -->
-        <div class="row d-flex align-items-center">
-
-            <!-- Grid column -->
-            <div class="col-md-7 col-lg-8">
-
-                <!--Copyright-->
-                <p class="text-center text-md-left">© 2020 Copyright:
-                    <a href="https://mdbootstrap.com/">
-                        <strong> La Bonne Planque</strong>
-                    </a>
-                </p>
-
-            </div>
-            <!-- Grid column -->
-
-            <!-- Grid column -->
-            <div class="col-md-5 col-lg-4 ml-lg-0">
-
-                <!-- Social buttons -->
-                <div class="text-center text-md-right">
-                    <ul class="list-unstyled list-inline">
-                        <li class="list-inline-item">
-                            <a class="btn-floating btn-sm rgba-white-slight mx-1">
-                                <i class="fab fa-facebook-f"></i>
-                            </a>
-                        </li>
-                        <li class="list-inline-item">
-                            <a class="btn-floating btn-sm rgba-white-slight mx-1">
-                                <i class="fab fa-twitter"></i>
-                            </a>
-                        </li>
-                        <li class="list-inline-item">
-                            <a class="btn-floating btn-sm rgba-white-slight mx-1">
-                                <i class="fab fa-google-plus-g"></i>
-                            </a>
-                        </li>
-                        <li class="list-inline-item">
-                            <a class="btn-floating btn-sm rgba-white-slight mx-1">
-                                <i class="fab fa-linkedin-in"></i>
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-
-            </div>
-            <!-- Grid column -->
-
-        </div>
-        <!-- Grid row -->
+      <!-- Grid column -->
+      <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
+        <h6 class="text-uppercase mb-4 font-weight-bold">Contact</h6>
+        <p>
+          <i class="fas fa-home mr-3"></i> 8 Rue d'hozier, 13002 Marseille</p>
+        <p>
+          <i class="fas fa-envelope mr-3"></i> info@labonneplanque.com</p>
+        <p>
+          <i class="fas fa-phone mr-3"></i> + 01 234 567 88</p>
+        <p>
+          <i class="fas fa-print mr-3"></i> + 01 234 567 89</p>
+      </div>
+      <!-- Grid column -->
 
     </div>
-    <!-- Footer Links -->
+    <!-- Footer links -->
+
+    <hr>
+
+    <!-- Grid row -->
+    <div class="row d-flex align-items-center">
+
+      <!-- Grid column -->
+      <div class="col-md-7 col-lg-8">
+
+        <!--Copyright-->
+        <p class="text-center text-md-left">© 2020 Copyright:
+          <a href="https://mdbootstrap.com/">
+            <strong> La Bonne Planque</strong>
+          </a>
+        </p>
+
+      </div>
+      <!-- Grid column -->
+
+      <!-- Grid column -->
+      <div class="col-md-5 col-lg-4 ml-lg-0">
+
+        <!-- Social buttons -->
+        <div class="text-center text-md-right">
+          <ul class="list-unstyled list-inline">
+            <li class="list-inline-item">
+              <a class="btn-floating btn-sm rgba-white-slight mx-1">
+                <i class="fab fa-facebook-f"></i>
+              </a>
+            </li>
+            <li class="list-inline-item">
+              <a class="btn-floating btn-sm rgba-white-slight mx-1">
+                <i class="fab fa-twitter"></i>
+              </a>
+            </li>
+            <li class="list-inline-item">
+              <a class="btn-floating btn-sm rgba-white-slight mx-1">
+                <i class="fab fa-google-plus-g"></i>
+              </a>
+            </li>
+            <li class="list-inline-item">
+              <a class="btn-floating btn-sm rgba-white-slight mx-1">
+                <i class="fab fa-linkedin-in"></i>
+              </a>
+            </li>
+          </ul>
+        </div>
+
+      </div>
+      <!-- Grid column -->
+
+    </div>
+    <!-- Grid row -->
+
+  </div>
+  <!-- Footer Links -->
 
 </footer>
 <!-- Footer -->
--- include/header.php
+++ include/header.php
@@ -1,143 +1,124 @@
-<?php
-include "libs/session.php";
-Session::init();
-$filepath = realpath(dirname(__FILE__));
-include_once($filepath . "/../libs/Database.php");
-include_once($filepath . "/../helpers/Format.php");
+<?php 
+	include "libs/session.php";
+	Session::init();
+	$filepath = realpath(dirname(__FILE__));
+	include_once ($filepath."/../libs/Database.php");
+	include_once ($filepath."/../helpers/Format.php");
 
-spl_autoload_register(function ($class) {
-    include_once "classes/" . $class . ".php";
-});
+	spl_autoload_register(function($class){
+		include_once "classes/".$class.".php";
+	});
 
-//creating object of classes
-$db = new Database();
-$fm = new Format();
-$pd = new Product();
-$ct = new Cart();
-$cat = new Category();
-$csmr = new Customer();
+	//creating object of classes
+	$db = new Database();
+	$fm = new Format();
+	$pd = new Product();
+	$ct = new Cart();
+	$cat= new Category();
+	$csmr = new Customer();
 ?>
 
 <?php
 //code for cache-control
-header("Cache-Control: no-cache, must-revalidate");
-header("Pragma: no-cache");
-header("Expires: Sat, 26 Jul 1997 05:00:00 GMT");
-header("Cache-Control: max-age=2592000");
+  header("Cache-Control: no-cache, must-revalidate");
+  header("Pragma: no-cache"); 
+  header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); 
+  header("Cache-Control: max-age=2592000");
 ?>
 
 <!DOCTYPE HTML>
 <head>
-    <title>La Bonne Planque</title>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-    <link href="src/css/style.css" rel="stylesheet" type="text/css" media="all"/>
-    <link href="src/css/menu.css" rel="stylesheet" type="text/css" media="all"/>
-    <script src="src/js/jquerymain.js"></script>
-    <script src="src/js/script.js" type="text/javascript"></script>
-    <script type="text/javascript" src="src/js/jquery-1.7.2.min.js"></script>
-    <script type="text/javascript" src="src/js/nav.js"></script>
-    <script type="text/javascript" src="src/js/move-top.js"></script>
-    <script type="text/javascript" src="src/js/easing.js"></script>
-    <script type="text/javascript" src="src/js/nav-hover.js"></script>
-    <link href='http://fonts.googleapis.com/css?family=Monda' rel='stylesheet' type='text/css'>
-    <link href='http://fonts.googleapis.com/css?family=Doppio+One' rel='stylesheet' type='text/css'>
-    <script type="text/javascript">
-        $(document).ready(function ($) {
-            $('#dc_mega-menu-orange').dcMegaMenu({rowItems: '4', speed: 'fast', effect: 'fade'});
-        });
-    </script>
+<title>La Bonne Planque</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<link href="src/css/style.css" rel="stylesheet" type="text/css" media="all"/>
+<link href="src/css/menu.css" rel="stylesheet" type="text/css" media="all"/>
+<script src="src/js/jquerymain.js"></script>
+<script src="src/js/script.js" type="text/javascript"></script>
+<script type="text/javascript" src="src/js/jquery-1.7.2.min.js"></script> 
+<script type="text/javascript" src="src/js/nav.js"></script>
+<script type="text/javascript" src="src/js/move-top.js"></script>
+<script type="text/javascript" src="src/js/easing.js"></script> 
+<script type="text/javascript" src="src/js/nav-hover.js"></script>
+<link href='http://fonts.googleapis.com/css?family=Monda' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Doppio+One' rel='stylesheet' type='text/css'>
+<script type="text/javascript">
+  $(document).ready(function($){
+    $('#dc_mega-menu-orange').dcMegaMenu({rowItems:'4',speed:'fast',effect:'fade'});
+  });
+</script>
 </head>
 <body>
-<div class="wrap">
-    <div class="header_top">
-        <div class="logo">
-            <ul>
-
-                <li>
-                    <h3>La Bonne Planque</h3>
-                </li>
-            </ul>
-        </div>
-        <div class="header_top_right">
-            <!--  <div class="search_box">
-                 <form>
-                     <input type="text" value="Search for Products" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Search for Products';}"><input type="submit" value="SEARCH">
-                 </form>
-             </div> -->
-            <div class="shopping_cart">
-                <div class="cart">
-                    <a href="cart.php" title="Voir le panier" rel="nofollow">
-                        <span class="cart_title">Panier</span>
-                        <span class="no_product">
+  <div class="wrap">
+		<div class="header_top">
+			<div class="logo">
+				<ul>
+					
+					<li>
+						<h3>La Bonne Planque</h3>
+					</li>
+				</ul>
+			</div>
+			  <div class="header_top_right">
+			   <!--  <div class="search_box">
+				    <form>
+				    	<input type="text" value="Search for Products" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Search for Products';}"><input type="submit" value="SEARCH">
+				    </form>
+			    </div> -->
+			    <div class="shopping_cart">
+					<div class="cart">
+						<a href="cart.php" title="Voir le panier" rel="nofollow">
+								<span class="cart_title">Panier</span>
+								<span class="no_product">
 									<?php
-                                    $getData = $ct->checkCartTable();
-                                    if ($getData) {
-                                        $sum = Session::get("cart");
-                                        $qty = Session::get("qty");
-                                        echo "Qté: " . $qty . " ~ " . $sum . " €";
-                                    } else {
-                                        echo "(Vide)";
-                                    }
-
-                                    ?>
+									$getData = $ct->checkCartTable();
+									if ($getData) {
+										$sum = Session::get("cart");
+										$qty = Session::get("qty");
+											echo "Qté: " .$qty. " ~ ".$sum." €";
+									}else{
+										echo "(Vide)";
+									}
+										
+									?>
 								</span>
-                    </a>
-                </div>
-            </div>
-            <?php
-            if (isset($_GET['csid'])) {
-                $delCart = $ct->delCustomerCart();
-                $delCompare = $pd->delCustomerCompare(Session::get('csid'));
-                Session::destroy();
-            }
-            ?>
-            <div class="login">
-                <?php
-                $login = Session::get('cslogin');
-                if ($login == true) {
-                    ?>
-                    <a href="?csid=<?php echo Session::get('csid'); ?>">Déconnexion</a>
+							</a>
+						</div>
+			      </div>
+	<?php
+		if (isset($_GET['csid'])) {
+			$delCart = $ct->delCustomerCart();
+			$delCompare = $pd->delCustomerCompare(Session::get('csid'));
+			Session::destroy();
+		}
+	?>
+		   <div class="login">
+		<?php
+			$login = Session::get('cslogin');
+			if ($login == true) {
+		?>
+			<a href="?csid=<?php echo Session::get('csid'); ?>">Déconnexion</a>
 
-                <?php } else { ?>
-                    <a href="login.php" title="Pour se connecter">Connexion</a>
-                <?php } ?>
+		<?php }else{  ?>
+			<a href="login.php" title="Pour se connecter">Connexion</a>
+	   <?php } ?>
+		   
+		   </div>
+		 <div class="clear"></div>
+	 </div>
+	 <div class="clear"></div>
+ </div>
+<div class="menu">
+	<ul id="dc_mega-menu-orange" class="dc_mm-orange">
+	  <li><a href="index.php">Accueil</a></li>
+	 <!--  <li><a href="topbrands.php">Top Brands</a></li> -->
+	   <?php
+	  	$ckCompare = $pd->checkCompareData(Session::get('csid'));
+	 	if ($ckCompare) { ?>
+		    <li><a href="compare.php" title="Pour comparer">Comparaison</a></li>
+	 	<?php } ?>
 
-            </div>
-            <div class="clear"></div>
-        </div>
-        <div class="clear"></div>
-    </div>
-    <div class="menu">
-        <ul id="dc_mega-menu-orange" class="dc_mm-orange">
-            <li><a href="index.php">Accueil</a></li>
-            <!--  <li><a href="topbrands.php">Top Brands</a></li> -->
-            <?php
-            $ckCompare = $pd->checkCompareData(Session::get('csid'));
-            if ($ckCompare) { ?>
-                <li><a href="compare.php" title="Pour comparer">Comparaison</a></li>
-            <?php } ?>
-
-            <?php
-            $ckwlist = $pd->checkWishlistData(Session::get('csid'));
-            if ($ckwlist) { ?>
-                <li><a href="wishlist.php" title="Voir la wishlist">Wishlist</a></li>
-            <?php } ?>
-
-            <?php
-            $chkCart = $ct->checkCartTable();
-            if ($chkCart) { ?>
-                <li><a href="cart.php">Panier</a></li>
-                <li><a href="payment.php" title="Pour payer">Paiement</a></li>
-            <?php } ?>
-
-            <?php
-            $csid = Session::get('csid');
-            $ckorder = $csmr->checkOrder($csid);
-            if ($ckorder) { ?>
-                <li><a href="orderdetails.php" title="Voir les commandes">Commandes</a></li>
-            <?php } ?>
-
+<<<<<<< HEAD
             <?php
             $userlogin = Session::get('cslogin');
             if ($userlogin == true) { ?>
@@ -161,33 +142,78 @@ header("Cache-Control: max-age=2592000");
             box-sizing: border-box;
             cursor: pointer;
         }
+=======
+	 	 <?php
+	  	$ckwlist = $pd->checkWishlistData(Session::get('csid'));
+	 	if ($ckwlist) { ?>
+		    <li><a href="wishlist.php" title="Voir la wishlist">Wishlist</a></li>
+	 	<?php } ?>
 
-        .cart {
-            height: 38px;
-            width: 10em;
-            padding: 0 0 0 10px;
-            background: #EDEDED;
-            border: 1px solid #CECECE;
-            border-left: none;
-            line-height: 36px;
-            -webkit-border-radius: 0 2px 2px 0;
-            border-radius: 0 2px 2px 0;
-            -webkit-box-sizing: border-box;
-            -moz-box-sizing: border-box;
-            box-sizing: border-box;
-        }
+	 	<?php 
+	 	$chkCart = $ct->checkCartTable();
+	 	if ($chkCart) { ?>
+	 		<li><a href="cart.php">Panier</a></li>
+	 		<li><a href="payment.php" title="Pour payer">Paiement</a></li>
+	 	<?php } ?>
+	  
+	  <?php 
+	  	$csid = Session::get('csid');
+	 	$ckorder = $csmr->checkOrder($csid);
+	 	if ($ckorder) { ?>
+	 		<li><a href="orderdetails.php" title="Voir les commandes">Commandes</a></li>
+	 	<?php } ?>
+>>>>>>> parent of eff28eb... MàJ
 
-        .login {
-            background: rgba(0, 0, 0, 0) url("../images/gradient_light-1.png") repeat-x scroll 0 0;
-            border: 1px solid #cecece;
-            box-sizing: border-box;
-            color: #444;
-            display: block;
-            float: left;
-            font-size: 27px;
-            height: 38px;
-            margin-left: 10px;
-            text-align: center;
-            width: 7em;
-        }
-    </style>
+	  <?php
+	  	$userlogin = Session::get('cslogin');
+	  	if ($userlogin == true) { ?>
+	  	<li><a href="profile.php" title="Voir le profil">Profil</a></li>
+	  <?php	} ?>
+	  
+	  <li><a href="contact.php" title="Pour nous contacter">Contact</a> </li>
+	  <div class="clear"></div>
+	</ul>
+</div>
+<style>
+	.shopping_cart{
+	float:left;
+	/*width: 202px;*/
+	position:relative;
+	margin-left:15px;
+	padding: 0 0 0 46px;
+	background: url(../images/header_cart.png) 0 0 no-repeat;
+	z-index: 99;
+	-webkit-box-sizing: border-box;
+	-moz-box-sizing: border-box;
+	box-sizing: border-box;
+	cursor:pointer;
+}
+	.cart{
+	height: 38px;
+	width : 10em;
+	padding: 0 0 0 10px;
+	background: #EDEDED;
+	border: 1px solid #CECECE;
+	border-left: none;
+	line-height: 36px;
+	-webkit-border-radius: 0 2px 2px 0;
+	border-radius: 0 2px 2px 0;
+	-webkit-box-sizing: border-box;
+	-moz-box-sizing: border-box;
+	box-sizing: border-box;
+}
+
+.login {
+	background: rgba(0, 0, 0, 0) url("../images/gradient_light-1.png") repeat-x scroll 0 0;
+	border: 1px solid #cecece;
+	box-sizing: border-box;
+	color: #444;
+	display: block;
+	float: left;
+	font-size: 27px;
+	height: 38px;
+	margin-left: 10px;
+	text-align: center;
+	width: 7em;
+}
+</style>
