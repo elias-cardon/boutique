652efde79672c6e95d18b43f0d6e5dc7dac19ea1
EliasCardon <62133614+EliasCardon@users.noreply.github.com>
2020-11-26 16:05:35 +0100

    MàJ

--- compare.php
+++ compare.php
@@ -1,57 +1,59 @@
 <?php
-	include("include/header.php");
+include("include/header.php");
 ?>
 <?php
-	$login = Session::get('cslogin');
-	if ($login == false) {
-		header("Location: login.php");
-	}
+$login = Session::get('cslogin');
+if ($login == false) {
+    header("Location: login.php");
+}
 ?>
- <div class="main">
+<div class="main">
     <div class="content">
-    	<div class="cartoption">		
-			<div class="cartpage">
-			    	<h2 style="width:100%;text-align:center">Comparaison</h2>
-						<table class="tblone">
-							<tr>
-								<th>N°</th>
-								<th>Nom du produit</th>
-								<th>Prix</th>
-								<th>Image</th>
-								<th>Action</th>
-							</tr>
-					    <?php
-					    	$csid = Session::get('csid');
-							$getProd = $pd->getComparedData($csid);
-							if ($getProd) {
-								$i = 1;
-								while ($cartpd = $getProd->fetch_assoc()) {
-					   	?>
-							<tr>
-								<td><?php echo $i?></td>
-								<td><?php echo $cartpd['productName'];?></td>
-								<td><?php echo $cartpd['price'];?> €</td>
-								<td><img src="admin/<?php echo $cartpd['image'];?>" alt=""/></td>
-								<td><a href="details.php?pdid=<?php echo $cartpd['productId'];?>">View</a></td>
-		
-							</tr>
+        <div class="cartoption">
+            <div class="cartpage">
+                <h2 style="width:100%;text-align:center">Comparaison</h2>
+                <table class="tblone">
+                    <tr>
+                        <th>N°</th>
+                        <th>Nom du produit</th>
+                        <th>Prix</th>
+                        <th>Image</th>
+                        <th>Action</th>
+                    </tr>
+                    <?php
+                    $csid = Session::get('csid');
+                    $getProd = $pd->getComparedData($csid);
+                    if ($getProd) {
+                        $i = 1;
+                        while ($cartpd = $getProd->fetch_assoc()) {
+                            ?>
+                            <tr>
+                                <td><?php echo $i ?></td>
+                                <td><?php echo $cartpd['productName']; ?></td>
+                                <td><?php echo $cartpd['price']; ?> €</td>
+                                <td><img src="admin/<?php echo $cartpd['image']; ?>" alt=""/></td>
+                                <td><a href="details.php?pdid=<?php echo $cartpd['productId']; ?>">View</a></td>
 
-						<?php $i++; } } ?>
+                            </tr>
 
-						</table>
-						
-					</div>
-					<div class="shopping" style="width:600px;margin:0 auto;">
-						<div class="shopleft">
-							<a href="index.php"> <img src="images/shop.png" alt="" /></a>
-						</div>
-					</div>
-    	</div>  	
-       <div class="clear"></div>
+                            <?php $i++;
+                        }
+                    } ?>
+
+                </table>
+
+            </div>
+            <div class="shopping" style="width:600px;margin:0 auto;">
+                <div class="shopleft">
+                    <a href="index.php"> <img src="images/shop.png" alt=""/></a>
+                </div>
+            </div>
+        </div>
+        <div class="clear"></div>
     </div>
- </div>
+</div>
 </div>
 
- <?php
-	include("include/footer.php");
+<?php
+include("include/footer.php");
 ?>
