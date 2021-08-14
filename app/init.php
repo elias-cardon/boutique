08dc27abe75fa50c22f4bb85773818445ee6365f
Elias Cardon <62133614+elias-cardon@users.noreply.github.com>
2021-08-14 18:22:20 +0200

    Création de fichiers

---
+++ app/init.php
@@ -0,0 +1,6 @@
+<?php
+//Sert à appeler tous les fichier dans le dossier app. On va y mettre que des include.
+include "../app/core/controller.php";
+include "../app/core/database.php";
+include "../app/core/functions.php";
+include "../app/core/app.php";
