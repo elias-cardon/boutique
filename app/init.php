bc3612e245f349d216619b95f41beb7f0c702c6c
Elias Cardon <62133614+elias-cardon@users.noreply.github.com>
2021-08-25 20:29:47 +0200

    On recommence

--- app/init.php
+++
@@ -1,6 +0,0 @@
-<?php
-//Sert à appeler tous les fichier dans le dossier app. On va y mettre que des include.
-include "../app/core/controller.php";
-include "../app/core/database.php";
-include "../app/core/functions.php";
-include "../app/core/app.php";
