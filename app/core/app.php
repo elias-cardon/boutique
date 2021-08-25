c4422609efcd4e82a771a807d7fa62c27b1bbead
Elias Cardon <62133614+elias-cardon@users.noreply.github.com>
2021-08-25 21:24:51 +0200

    Création de fichiers htaccess + de fichiers backend + class App

---
+++ app/core/app.php
@@ -0,0 +1,17 @@
+<?php
+
+class App
+{
+    protected $controller = "home";
+    protected $method = "index";
+
+    function __construct(){
+        $url = $this->parseURL();
+        show($url);
+    }
+
+    private function parseURL(){
+        $url = isset($_GET['url']) ? $_GET['url'] : "home";
+        return explode("/", filter_var(trim($url, "/"), FILTER_SANITIZE_URL));
+    }
+}
--- public/index.php
+++ public/index.php
@@ -1,1 +1,6 @@
 <?php
+
+session_start();
+include "../app/init.php";
+
+$app = new App();
